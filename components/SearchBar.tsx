7b9d45b56605e4194d835539be303ad3681f504e
Ahmet Musa Çatak <ahmet.catak@tubitak.gov.tr>
2023-08-17 15:49:39 +0300

    SearchBar is update params

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,7 +1,8 @@
 "use client"
 
 import { SearchManufacturer } from "./"
-import { useState } from 'react'
+import React, { useState } from 'react'
+import { useRouter } from "next/navigation"
 import Image from 'next/image'
 
 const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
@@ -21,9 +22,40 @@ const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
 const SearchBar = () => {
     const [manufacturer, setManufacturer] = useState('');
     const [model, setModel] = useState('');
-    const handleSearch = () => {
+    const router = useRouter();
+    const handleSearch = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        if (manufacturer === '' || model === '') {
+            return alert('Please fill in all fields');
+        }
+
+        updateSearchParams(manufacturer.toLowerCase(), model.toLowerCase());
 
     }
+
+    const updateSearchParams = (manufacturer: string, model: string) => {
+        const searchParams = new URLSearchParams(window.location.search);
+        if (model) {
+            searchParams.set('model', model);
+        }
+        else {
+            searchParams.delete('model');
+        }
+        if (manufacturer) {
+            searchParams.set('manufacturer', manufacturer);
+        }
+        else {
+            searchParams.delete('manufacturer');
+        }
+
+        const newPathName = `${window.location.pathname}?${searchParams.toString()}`;
+
+        router.push(newPathName);
+
+        return searchParams.toString();
+    }
+
     return (
         <form className='searchbar'
             onSubmit={handleSearch}>
@@ -35,16 +67,16 @@ const SearchBar = () => {
                 <SearchButton otherClasses="sm:hidden" />
             </div>
             <div className="searchbar__item">
-                <Image src="/modal-icon.png"
+                <Image src="/model-icon.png"
                     width={25}
                     height={25}
-                    className="absoloute w-[20px] h-[20px] ml-4"
+                    className="absolute w-[20px] h-[20px] ml-4"
                     alt="car model"
                 />
 
                 <input
                     type="text"
-                    name="modal"
+                    name="model"
                     value={model}
                     onChange={(e) => setModel(e.target.value)}
                     placeholder="Tiguan"
